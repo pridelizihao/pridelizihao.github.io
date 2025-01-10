132d84ce50ae4646f4c186cad057d481ded63c1a
pridelizihao <pridelzh@foxmail.com>
2025-01-10 12:06:33 +0800

    Site updated: 2025-01-10 12:06:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/09/hello-world/"];function toRandomPost(){
+var posts=["posts/4a17b156/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
